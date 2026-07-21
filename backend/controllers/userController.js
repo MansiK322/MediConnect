0c712332faa0df1c0af6e5472bea3af690e5530c
Mansi Kanwal <mansikanwal19@gmail.com>
2026-07-21 14:02:55 +0530

    Make my-appointment Page Dynamic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -205,4 +205,64 @@ const bookAppointment = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser, getProfile, updateProfile, bookAppointment };
+// API to get user appointments for My Appointments page
+
+const listAppointment = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const appointments = await appointmentModel.find({ userId });
+
+    res.json({
+      success: true,
+      appointments,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//API to cancel appointment
+
+const cancelAppointment = async(req,res)=>{
+
+  try {
+    const userId = req.userId;
+    const { appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId)
+
+    //verify appointment user
+
+    if(appointmentData.userId.toString() !== userId){
+      return res.json({success:false, message:"Unauthorized action"})
+    }
+
+    await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled: true})
+
+    //releasing doctor slot
+
+    const {docId, slotDate, slotTime} = appointmentData
+
+    const doctorData = await doctorModel.findById(docId)
+
+    let slots_booked = doctorData.slots_booked
+
+    slots_booked[slotDate] = slots_booked[slotDate].filter(e => e!== slotTime)
+
+    await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+    res.json({success:true, message:"Appointment Cancelled"})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+} 
+
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment};
